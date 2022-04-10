e6c605de05bb13d76278a8d94da8d4376b0634ff
Jason <jasoncarrillo0@gmail.com>
2022-04-10 16:28:13 +0300

    redux store update

--- src/redux/dictionary/dictReducer.js
+++ src/redux/dictionary/dictReducer.js
@@ -4,11 +4,41 @@ export const dictionaryReducer = (state = {}, action) => {
     switch (action.type) {
         case DICT_ACTION_TYPES.SET_DICTIONARY:
             return {
-                ...action.payload
-            }
+                ...action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_ADJECTIVES:
+            return {
+                ...state,
+                adjectives: action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_CONNECTORS:
+            return {
+                ...state,
+                connectors: action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_NOUNS:
+            return {
+                ...state,
+                nouns: action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_PARTICLES:
+            return {
+                ...state,
+                particles: action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_PREPOSITIONS:
+            return {
+                ...state,
+                prepositions: action.payload,
+            };
+        case DICT_ACTION_TYPES.SET_ALL_VERBS:
+            return {
+                ...state,
+                verbs: action.payload,
+            };
         default:
             return {
-                ...state
-            }
+                ...state,
+            };
     }
 }
