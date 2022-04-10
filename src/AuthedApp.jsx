0eba55367e3d8ce4cbafc5d78bf3cd96540f81fc
Jason <jasoncarrillo0@gmail.com>
2022-04-10 12:02:47 +0300

    added dictionary query on AuthedApp load

--- src/AuthedApp.jsx
+++ src/AuthedApp.jsx
@@ -6,10 +6,37 @@ import HomePage from "./components/HomePage";
 import ProfileInfo from "./components/ProfileInfo";
 import PrivateRoute from './components/reusable/PrivateRoute'
 import { useAuth } from "./contexts/AuthContext";
-import { AUTHORIZED_EMAILS } from "./helpers/constants";
+import { AUTHORIZED_EMAILS, DICT_FIREBASE_ID, ERR_SNACKBAR, UPLOAD_WORDS } from "./helpers/constants";
+import { useEffect } from "react";
+import { doc, getDocFromServer, query, collection, getDocsFromServer } from "firebase/firestore";
+import { db } from "./firebase/firebase";
+import { useSnackbar } from "notistack";
 function AuthedApp() {
     const { currUser } = useAuth();
     const authed       = AUTHORIZED_EMAILS.includes(currUser.email);
+    const { enqueueSnackbar } = useSnackbar();
+    useEffect(() => {
+        async function loadDictionary() {
+            try {
+                const dict = {};
+                for (const collectionName of Object.keys(UPLOAD_WORDS)) {
+                    const dbQuery  = query(collection(db, 'dictionary', DICT_FIREBASE_ID, collectionName));
+                    const snapshot = await getDocsFromServer(dbQuery);
+                    const wordDocs = [];
+
+                    for (const document of snapshot.docs) {
+                        wordDocs.push({id: document.id, ...document.data()})
+                    }
+                    dict[collectionName] = wordDocs;
+                }
+                console.log(dict);
+            } catch (e) {
+                console.log(e);
+                enqueueSnackbar(e.message, ERR_SNACKBAR)
+            }
+        }
+        loadDictionary();
+    }, [])
     return (
         <Switch>
             <Route exact path="/home" component={HomePage}/>
