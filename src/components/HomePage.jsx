50692d56d0264881ec7423537c6d49dad95a07d5
Jason <jasoncarrillo0@gmail.com>
2022-04-27 16:56:10 +0300

    styled home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import s from './HomePage.module.scss';
 import { Redirect } from 'react-router-dom'
+import CoffeeTableSvg from './HomePage/CoffeeTableSvg';
 const HomePage = ({}) => {
-    const { currUser, signup } = useAuth();
+    const { currUser } = useAuth();
 
     if (!currUser) return <Redirect to="/signup"/>
     
     return (
         <div className={s.wrap}>
-            Welcome!
+            <h1>Let's get to work!</h1>
+            <CoffeeTableSvg/>
         </div>
     )
 };
